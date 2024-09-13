e2b69a3a93c99d3be98004d966a9492375f1aef0
ashishsuthar96 <80204112+ashishsuthar96@users.noreply.github.com>
2024-09-13 16:20:26 +0000

    nav toggel

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
+let menu = document.querySelector(".menu-links");
+let icon = document.querySelector(".hamburger-icon");
+
 function toggleMenu() {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+    menu.classList.toggle('open');
+    icon.classList.toggle('open');
   }
   
